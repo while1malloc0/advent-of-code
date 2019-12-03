5ee81d2798b633cd4b1eeab4c4f2afa6803af5a6
John Turner <while1malloc0@gmail.com>
2019-12-03 04:17:08 +0000

    Add day 1

--- 2019/day1/main.go
+++ 2019/day1/main.go
@@ -1,7 +1,68 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	f, err := os.Open("input")
+	defer f.Close()
+	if err != nil {
+		panic(err)
+	}
+	firstPart(f)
+	f, err = os.Open("input")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	secondPart(f)
+}
+
+func firstPart(f *os.File) {
+	r := bufio.NewScanner(f)
+	var sum int64
+	for r.Scan() {
+		text := r.Text()
+		text = strings.TrimSpace(text)
+		num, err := strconv.ParseInt(text, 0, 0)
+		if err != nil {
+			panic(err)
+		}
+		sum += int64(math.Floor(float64(num/3)) - 2)
+	}
+	fmt.Println(sum)
+}
+
+func secondPart(f *os.File) {
+	r := bufio.NewScanner(f)
+	var sum int64
+	for r.Scan() {
+		text := r.Text()
+		text = strings.TrimSpace(text)
+		num, err := strconv.ParseInt(text, 0, 0)
+		if err != nil {
+			panic(err)
+		}
+		sum += getFuelWithSelf(num)
+	}
+	fmt.Println(sum)
+}
+
+func getFuelWithSelf(n int64) int64 {
+	rem := int64(math.Floor(float64(n/3)) - 2)
+	sum := rem
+	for {
+		rem = int64(math.Floor(float64(rem/3)) - 2)
+		if rem < 1 {
+			break
+		}
+		sum += rem
+	}
+	return int64(sum)
 }
---
+++ 2019/day1/main_test.go
@@ -0,0 +1,23 @@
+package main
+
+import "testing"
+
+func TestGetFuelWithSelf(t *testing.T) {
+	cases := []struct {
+		name  string
+		input int64
+		want  int64
+	}{
+		{"14", 14, 2},
+	}
+
+	for _, tc := range cases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			got := getFuelWithSelf(tc.input)
+			if got != tc.want {
+				t.Fatalf("[%s]: Got: %d, Want: %d", tc.name, got, tc.want)
+			}
+		})
+	}
+}
