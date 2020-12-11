3b8af0083dc6e4872cbe407bbaf85f04e2ea5c7e
John Turner <jturner@localhost.localdomain>
2020-12-11 08:30:51 -0500

    Day 10 part 2

--- 2020/day10/main.go
+++ 2020/day10/main.go
@@ -30,6 +30,33 @@ func countOnesAndThrees(in []int) (int, int) {
 	return ones, threes
 }
 
+func findNumPaths(in []int) int {
+	sorted := []int{0}
+	for _, n := range in {
+		sorted = append(sorted, n)
+	}
+	sort.Ints(sorted)
+	sorted = append(sorted, sorted[len(sorted)-1]+3)
+
+	paths := map[int]int{}
+	paths[0] = 1
+	for _, num := range sorted {
+		if num == 0 {
+			continue
+		}
+		if val, ok := paths[num-1]; ok {
+			paths[num] += val
+		}
+		if val, ok := paths[num-2]; ok {
+			paths[num] += val
+		}
+		if val, ok := paths[num-3]; ok {
+			paths[num] += val
+		}
+	}
+	return paths[sorted[len(sorted)-1]]
+}
+
 func main() {
 	var in []int
 	challenge.InputScanFunc("input", func(s string) error {
@@ -47,5 +74,11 @@ func main() {
 		return nil
 	}
 
-	challenge.Run(partOneFunc, nil)
+	partTwoFunc := func() error {
+		result := findNumPaths(in)
+		fmt.Println(result)
+		return nil
+	}
+
+	challenge.Run(partOneFunc, partTwoFunc)
 }
