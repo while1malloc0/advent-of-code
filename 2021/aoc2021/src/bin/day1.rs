90c99654359f6a718e6cf50d4c3701ba1e906bc9
John Turner <johnturner@me.com>
2021-12-01 23:44:21 +0000

    2021: add clever/obtuse solutions for day 2

--- 2021/aoc2021/src/bin/day1.rs
+++ 2021/aoc2021/src/bin/day1.rs
@@ -1,6 +1,6 @@
 fn main() {
-    let input_raw = include_str!("../../inputs/1.1.txt").to_string();
-    let input: Vec<i32> = input_raw
+    let input: Vec<i32> = include_str!("../../inputs/1.1.txt")
+        .to_string()
         .trim()
         .split("\n")
         .map(|i| i.parse::<i32>().expect("could not parse input to int"))
@@ -8,28 +8,50 @@ fn main() {
 
     {
         println!("Part 1:");
+        // non-clever solution, just use a for-loop
         let mut count = 0;
         for i in 0..input.len() - 1 {
             if input[i] < input[i + 1] {
                 count += 1;
             }
         }
-        println!("{:?}", count);
+        println!("Simple: {:?}", count);
+
+        // clever (or maybe just obtuse?) solution, slice input into a sliding
+        // 2-window, filter for tuples that match our criteria, and see how many
+        // there are
+        let opt = input
+            .clone()
+            .as_slice()
+            .windows(2)
+            .filter(|x| x[0] < x[1])
+            .count();
+        println!("Clever: {:?}", opt);
     }
 
     {
         println!("Part 2:");
-        let input_windows = input[..].windows(3);
-        let mut v = vec![];
-        for window in input_windows {
-            v.push(window);
-        }
+
+        // Simple: use a for loop
+        let v: Vec<&[i32]> = input.as_slice().windows(3).collect();
         let mut count = 0;
         for i in 0..v.len() - 1 {
             if v[i].iter().sum::<i32>() < v[i + 1].iter().sum::<i32>() {
                 count += 1
             }
         }
-        println!("{:?}", count);
+        println!("Simple: {:?}", count);
+
+        // Clever (again, or maybe just obtuse): repeat our slice trick above
+        let opt = input
+            .as_slice()
+            .windows(3)
+            .map(|x| x.iter().sum()) // transform into a slice of sums...
+            .collect::<Vec<i32>>()
+            .as_slice()
+            .windows(2) // ...then repeat the same trick as above
+            .filter(|x| x[0] < x[1])
+            .count();
+        println!("Clever: {:?}", opt);
     }
 }
