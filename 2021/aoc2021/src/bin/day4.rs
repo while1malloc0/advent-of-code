fa1db6aef2fa49941e866e8849a2ee7e75caffd2
John Turner <johnturner@me.com>
2021-12-05 03:42:40 +0000

    2021: day 4 part 1

--- 2021/aoc2021/src/bin/day4.rs
+++ 2021/aoc2021/src/bin/day4.rs
@@ -1,5 +1,13 @@
 fn main() {
-    println!("working!");
+    let mut game: BingoGame = include_str!("../../inputs/4.txt").into();
+    let (mut winner, winning_number) = game.play().unwrap();
+    let answer: u32 = winner.uncalled().map(|x| x.val).sum::<u32>() * winning_number;
+    println!("Part 1: {}", answer);
+
+    let mut p2: BingoGame = include_str!("../../inputs/4.txt").into();
+    let (mut p2_winner, p2_winning_number) = p2.play_to_lose().unwrap();
+    let p2_answer: u32 = p2_winner.uncalled().map(|x| x.val).sum::<u32>() * p2_winning_number;
+    println!("Part 2: {}", p2_answer);
 }
 
 struct BingoGame {
@@ -9,15 +17,40 @@ struct BingoGame {
 
 impl BingoGame {
     fn play(&mut self) -> Result<(BingoCard, u32), &str> {
-        for val in self.uncalled_nums {
-            for card in self.cards {
-                let winner: bool = card.mark(val);
-                if winner {
-                    return Ok((card, val));
+        for val in self.uncalled_nums.iter() {
+            for i in 0..self.cards.len() {
+                self.cards[i].mark(*val);
+                if self.cards[i].won() {
+                    return Ok((self.cards[i].clone(), *val));
                 }
             }
         }
         Err("no winner found")
+    }
+
+    fn play_to_lose(&mut self) -> Result<(BingoCard, u32), &str> {
+        let mut winners = vec![];
+        let mut cards = self.cards.clone();
+        println!("Starting len: {}", cards.len());
+        for val in self.uncalled_nums.iter() {
+            let mut to_remove = vec![];
+            for i in 0..cards.len() {
+                cards[i].mark(*val);
+                if cards[i].won() {
+                    winners.push((cards[i].clone(), *val));
+                    to_remove.push(i);
+                }
+            }
+            println!("{:?}", to_remove);
+            for i in to_remove {
+                cards.remove(i);
+            }
+        }
+        if winners.is_empty() {
+            return Err("no winner found");
+        }
+        let (winner, winning_number) = &winners[winners.len() - 1];
+        Ok((winner.clone(), winning_number.clone()))
     }
 }
 
@@ -33,7 +66,7 @@ impl From<&str> for BingoGame {
             .map(|x| BingoCard::from(*x))
             .collect();
         BingoGame {
-            cards,
+            cards: cards,
             uncalled_nums: unparsed_nums,
         }
     }
@@ -45,11 +78,11 @@ struct BingoCard {
 }
 
 impl BingoCard {
-    fn rows(&self) -> impl Iterator<Item = &Vec<BingoSpace>> {
-        self.spaces.iter()
+    fn rows(&mut self) -> Vec<Vec<BingoSpace>> {
+        self.spaces.clone()
     }
 
-    fn cols(&self) -> impl Iterator<Item = Vec<BingoSpace>> {
+    fn cols(&mut self) -> Vec<Vec<BingoSpace>> {
         let num_cols = self.spaces[0].len();
         let mut result: Vec<Vec<BingoSpace>> = vec![];
         for i in 0..num_cols {
@@ -59,30 +92,32 @@ impl BingoCard {
             }
             result.push(row);
         }
-        result.into_iter()
+        result
     }
 
-    fn uncalled(&self) -> impl Iterator<Item = BingoSpace> {
+    fn uncalled(&mut self) -> impl Iterator<Item = BingoSpace> {
         let mut uncalled: Vec<BingoSpace> = vec![];
         for row in self.rows() {
             for space in row {
                 if !space.called {
-                    uncalled.push(*space);
+                    uncalled.push(space);
                 }
             }
         }
         uncalled.into_iter()
     }
 
-    fn mark(&mut self, val: u32) -> bool {
-        for row in self.rows() {
-            for space in row {
-                if space.val == val {
-                    space.called = true;
+    fn mark(&mut self, val: u32) {
+        for i in 0..self.spaces.len() {
+            for j in 0..self.spaces[i].len() {
+                if self.spaces[i][j].val == val {
+                    self.spaces[i][j].called = true
                 }
             }
         }
+    }
 
+    fn won(&mut self) -> bool {
         for row in self.rows() {
             if row.iter().all(|space| space.called) {
                 return true;
@@ -127,7 +162,18 @@ mod test {
     fn p1_e2e() {
         let mut game: BingoGame = include_str!("../../inputs/4.example.txt").into();
         let want = 4512;
-        let (winner, winning_number) = game.play().unwrap();
+        let (mut winner, winning_number) = game.play().unwrap();
+        assert_eq!(
+            winner.uncalled().map(|x| x.val).sum::<u32>() * winning_number,
+            want
+        );
+    }
+
+    #[test]
+    fn p2_e2e() {
+        let mut game: BingoGame = include_str!("../../inputs/4.example.txt").into();
+        let want = 1924;
+        let (mut winner, winning_number) = game.play_to_lose().unwrap();
         assert_eq!(
             winner.uncalled().map(|x| x.val).sum::<u32>() * winning_number,
             want
@@ -144,7 +190,7 @@ mod test {
  1 12 20 15 19
 "
         .trim();
-        let subject: BingoCard = input.into();
+        let mut subject: BingoCard = input.into();
         let want = 300;
         let got: u32 = subject.uncalled().map(|x| x.val).sum();
         assert_eq!(got, want);
