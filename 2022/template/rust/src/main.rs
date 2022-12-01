e93f36acc812871abc1def83c0920e74553d5d4c
John Turner <johnturner@me.com>
2022-12-01 08:54:27 -0500

    update range in template to accept the number 2

--- 2022/template/rust/src/main.rs
+++ 2022/template/rust/src/main.rs
@@ -1,14 +1,22 @@
+use clap::Parser;
 use std::fs;
 use std::path::PathBuf;
-use clap::Parser;
 
 #[derive(Parser)]
 #[command()]
 struct CLI {
-    #[arg(value_parser = clap::value_parser!(u16).range(1..2))]
+    #[arg(value_parser = clap::value_parser!(u16).range(1..3))]
     part: u16,
     #[arg(short, long)]
     example: bool,
+}
+
+fn read_data_file(filename: &str) -> String {
+    let in_file_path = PathBuf::from(format!("../data/{}", filename));
+    let in_file = fs::canonicalize(&in_file_path).expect("could not canonicalize file");
+    fs::read_to_string(in_file)
+        .expect("could not read file")
+        .to_string()
 }
 
 fn part_one(input: String) -> String {
@@ -27,9 +35,7 @@ fn main() {
         in_file_name = "example.txt";
     }
 
-    let in_file_path = PathBuf::from(format!("../data/{}", in_file_name));
-    let in_file = fs::canonicalize(&in_file_path).expect("could not canonicalize file");
-    let input = fs::read_to_string(in_file).expect("could not read file").to_string();
+    let input = read_data_file(in_file_name);
 
     if cli.part == 1 {
         println!("{}", part_one(input))
