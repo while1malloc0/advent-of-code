6df583d470488c9b06381bae7868486657b17394
John Turner <johnturner@me.com>
2022-12-04 20:31:24 -0500

    update template to not cause crate name clashes

--- 2022/scripts/create_day.sh
+++ 2022/scripts/create_day.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 
 cd "$(dirname "$0")/.." || exit 1
 
-cp -r template "day$1"
+day="day$1"
+cp -r template "$day"
+sed -i'' "s:replacewithday:$day:g" "$day/Cargo.toml"
 
 # TODO: fetch input using curl or something
