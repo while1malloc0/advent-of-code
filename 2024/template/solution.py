c07afba99c423c0ae4c555b0b2b3dd4f13cf470b
John Turner <jturner@squarespace.com>
2024-12-02 23:03:28 -0500

    2024: add template

---
+++ 2024/template/solution.py
@@ -0,0 +1,15 @@
+def p1(input: str) -> int:
+    with open(input) as content:
+        pass
+
+
+def p2(input: str) -> int:
+    with open(input) as content:
+        pass
+
+
+if __name__ == "__main__":
+    part1 = p1("input.txt")
+    print("Part1: {}".format(part1))
+    part2 = p2("input.txt")
+    print("Part2: {}".format(part2))
---
+++ 2024/template/solution_test.py
@@ -0,0 +1,15 @@
+from .solution import *
+
+FIXME = None
+
+
+def test_e2e_p1():
+    got = p1("example.txt")
+    want = FIXME
+    assert got == want
+
+
+def test_e2e_p2():
+    got = p2("example.txt")
+    want = FIXME
+    assert want == got
